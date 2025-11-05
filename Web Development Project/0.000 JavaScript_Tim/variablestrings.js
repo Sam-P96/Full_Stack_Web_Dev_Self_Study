390d41c8a1319ffc957992552ed8a39b93eda4c8
Samnap Peo <Samnappeo@gmail.com>
2025-11-05 10:51:03 +0200

    Initial commit

---
+++ Web Development Project/0.000 JavaScript_Tim/variablestrings.js
@@ -0,0 +1,35 @@
+//this is a single line comment
+/* this is a multiline comment
+this is another line, holy crap! */ 
+
+let myGreeting = "Hello from JavaScript!"; // This is my first variable
+
+let firstName;
+firstName= "John";
+
+let lastName;
+lastName = "Smith";
+
+let fullName = "Samnap Peo"
+//creat the output
+console.log("Hello " + firstName + " " + lastName + ", you got this to work!")
+console.log("Congratuations! You are now " + fullName)
+
+let num1 = 15;
+
+let num2 = "15";
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 / num2)
+console.log(num1 * num2)
+
+let isEqual;
+
+isEqual = num1 == num2;
+isEqual2 = num1 === num2;
+isEqual3 = num1 !== num2; // You can use >= but not >== 
+
+console.log(isEqual)
+console.log(isEqual2)
+console.log(isEqual3)
